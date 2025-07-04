55a49bd2c9a7814ce3b72d5b89427dbfe3cedab9
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-07-05 00:20:32 +0600

    Add video section and enhance styling in index.html and style.css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function videoanimation() {
+    
 var videoContainer = document.querySelector("#video-container");
 var playBtn = document.querySelector("#play");
 videoContainer.addEventListener("mouseenter",
@@ -18,3 +20,21 @@ videoContainer.addEventListener("mousemove",function(dets){
         top:dets.y-30
 })}
 )
+}
+videoanimation();
+function loadinganimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+loadinganimation();
